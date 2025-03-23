414fbc18b70f0da76a73ccd85c26d9b2daad8d7d
Ashton Pomerantz <ashtonpomerantz.com>
2025-03-22 19:34:16 -0700

    changing db.php

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
-    $servername = "sql3.freesqldatabase.com";
-    $username = "sql3766137";
-    $password = "aLjTI6rH5A";
-    $database = "sql3766137";
+    $host = "ecommerce.c4xw6gema9h1.us-east-1.rds.amazonaws.com";
+    $user = "admin";
+    $password = "db.Tr33P0st";
+    $dbname = "ecommerce";
 
-    $con = mysqli_connect("$servername","$username","$password","$database");
+    $con = mysqli_connect("$host","$user","$password","$dbname");
 
     // Check connection
     if (mysqli_connect_errno()){
