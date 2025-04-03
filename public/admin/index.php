69de49bc3d7d2f19716a547d4772f59db90d2f0d
x1Wolf101x <amsorensen33@gmail.com>
2025-04-03 15:33:45 -0700

    4-3-2025 Update 1
    Admin dashboard added. Basic elements of User dashboard added.

---
+++ public/admin/index.php
@@ -0,0 +1,106 @@
+<?php
+
+include('includes/header.php');
+
+?>
+
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-12">
+
+            <div class="row">
+        <div class="ms-3">
+          <h3 class="mb-0 h4 font-weight-bolder">Dashboard</h3>
+          <p class="mb-4">
+            
+          </p>
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+          <div class="card">
+            <div class="card-header p-2 ps-3">
+              <div class="d-flex justify-content-between">
+                <div>
+                  <p class="text-sm mb-0 text-capitalize"></p>
+                  <h4 class="mb-0"></h4>
+                </div>
+                <div class="icon icon-md icon-shape bg-gradient-dark shadow-dark shadow text-center border-radius-lg">
+                  <i class="material-symbols-rounded opacity-10"></i>
+                </div>
+              </div>
+            </div>
+            <hr class="dark horizontal my-0">
+            <div class="card-footer p-2 ps-3">
+              <p class="mb-0 text-sm"><span class="text-success font-weight-bolder"> </span></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+          <div class="card">
+            <div class="card-header p-2 ps-3">
+              <div class="d-flex justify-content-between">
+                <div>
+                  <p class="text-sm mb-0 text-capitalize"></p>
+                  <h4 class="mb-0"></h4>
+                </div>
+                <div class="icon icon-md icon-shape bg-gradient-dark shadow-dark shadow text-center border-radius-lg">
+                  <i class="material-symbols-rounded opacity-10"></i>
+                </div>
+              </div>
+            </div>
+            <hr class="dark horizontal my-0">
+            <div class="card-footer p-2 ps-3">
+              <p class="mb-0 text-sm"><span class="text-success font-weight-bolder"> </span></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+          <div class="card">
+            <div class="card-header p-2 ps-3">
+              <div class="d-flex justify-content-between">
+                <div>
+                  <p class="text-sm mb-0 text-capitalize"></p>
+                  <h4 class="mb-0"></h4>
+                </div>
+                <div class="icon icon-md icon-shape bg-gradient-dark shadow-dark shadow text-center border-radius-lg">
+                  <i class="material-symbols-rounded opacity-10"></i>
+                </div>
+              </div>
+            </div>
+            <hr class="dark horizontal my-0">
+            <div class="card-footer p-2 ps-3">
+              <p class="mb-0 text-sm"><span class="text-danger font-weight-bolder"> </span></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-sm-6">
+          <div class="card">
+            <div class="card-header p-2 ps-3">
+              <div class="d-flex justify-content-between">
+                <div>
+                  <p class="text-sm mb-0 text-capitalize"></p>
+                  <h4 class="mb-0"></h4>
+                </div>
+                <div class="icon icon-md icon-shape bg-gradient-dark shadow-dark shadow text-center border-radius-lg">
+                  <i class="material-symbols-rounded opacity-10"></i>
+                </div>
+              </div>
+            </div>
+            <hr class="dark horizontal my-0">
+            <div class="card-footer p-2 ps-3">
+              <p class="mb-0 text-sm"><span class="text-success font-weight-bolder"> </span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+        </div>
+    </div>
+
+</div>
+
+<?php
+
+include('includes/footer.php');
+
+?>
--- public/user/account.php
+++ public/user/account.php
@@ -1,8 +1,32 @@
 
-<?php include $_SERVER['DOCUMENT_ROOT'] . '/../includes/header.php'; ?>
+<?php 
+include $_SERVER['DOCUMENT_ROOT'] . '/../includes/header.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/../config/db.php';
+
+//Select information from users table to display on dashboard
+$sql_users = "SELECT first_name, last_name
+        FROM users";
+$result_users = $conn->query($sql_users);
+$row_users = $result_users->fetch_assoc()
+
+//query might need to go here to pull information from user_address table
+//I need to look at Amazon and Newegg to see how they format their user dashboards
+?>
 
 <body>
     <main>
+
+        <div class="container-fluid bg-light py-5">
+            <div class="m-auto text-left col-md-6">
+                <h1 class="h1">User Dashboard</h1>
+                <h4>User: </h4>
+                <p> 
+                    <?php echo htmlspecialchars($row_users['first_name']); ?> <?php echo htmlspecialchars($row_users['last_name']); ?>
+                </p>
+                
+            </div>
+        </div>
+
     </main>
 </body>
 
