b691fa58442cad565abd25bc3f6fa17366c1d052
Ashton Pomerantz <ashtonpomerantz@gmail.com>
2025-04-30 12:45:25 -0700

    fix carousel

--- public/index.php
+++ public/index.php
@@ -14,8 +14,8 @@ $products = $conn->query("SELECT name, image_path FROM products ORDER BY created
 
         <!-- Carousel Section -->
         <div id="productCarousel" class="carousel slide mx-auto mt-4" data-bs-ride="carousel" style="max-width: 75%; overflow: hidden">
-            <div class="carousel-inner">
 
+            <div class="carousel-inner">
                 <?php
                 $active = true;
                 while ($product = $products->fetch_assoc()):
@@ -29,24 +29,8 @@ $products = $conn->query("SELECT name, image_path FROM products ORDER BY created
                 </div>
                 <?php $active = false; ?>
                 <?php endwhile; ?>
-
-                <!--<div class="carousel-item">-->
-                <!--    <img src="/assets/images/uploads/.placeholder.png" class="d-block w-100" alt="Product 2">-->
-                <!--    <div class="carousel-caption d-none d-md-block">-->
-                <!--        <h5>Featured Product 2</h5>-->
-                <!--        <a href="/shop/shop.php" class="btn btn-primary">Shop Now</a>-->
-                <!--    </div>-->
-                <!--</div>-->
-                <!---->
-                <!--<div class="carousel-item">-->
-                <!--    <img src="/assets/images/uploads/.placeholder.png" class="d-block w-100" alt="Product 3">-->
-                <!--    <div class="carousel-caption d-none d-md-block">-->
-                <!--        <h5>Featured Product 3</h5>-->
-                <!--        <a href="/shop/shop.php" class="btn btn-primary">Shop Now</a>-->
-                <!--    </div>-->
-                <!--</div>-->
-
             </div>
+
             <button class="carousel-control-prev" type="button" data-bs-target="#productCarousel" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
